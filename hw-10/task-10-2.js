4385c0646531c3b161e9015ac32509849e6221a3
Viktoriia Yahupa <viktoria_yagupa@hotmail.com>
2025-02-15 02:47:13 +0200

    homework 10 done

--- hw-10/task-10-2.js
+++ hw-10/task-10-2.js
@@ -1,15 +1,23 @@
 let cache = new Map();
 
 function sqr(x, cache) {
+
+    //1)  перевіряємо, чи є в cache вже обчислений квадрат від х
     if (cache.has(x)) {
         console.log('Знаходимо в кеші вже доданий раніше квадрат числа ' + x);
+
+        //2) повертаємо існуючий запис з cache в разі його наявності
         return cache.get(x);
     }
 
+    //3)обчислюємо квадрат від х в разі його відсутності в cache
     console.log('Обчислюємо квадрат числа ' + x);
     const result = x * x;
 
+    //4)записуємо в cache обчислений квадрат від х в разі його відсутності в cache
     cache.set(x, result)
+
+    //5)повертаємо новий запис з cache
     return result;
 }
 
